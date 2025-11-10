bd5d037cc147f4cae2dc44a3a0a4ea9f98d7ac02
binayaktripathy21-sketch <binayaktripathy21@gmail.com>
2025-11-10 19:42:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve your index.html file when someone visits "/"
 app.get("/", (req, res) => {
-  res.send("Hello from Render!");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
